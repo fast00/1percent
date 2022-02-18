08e382fec4b4120ee3673b61af79e1086d64874e
fast00 <74058894+fast00@users.noreply.github.com>
2022-02-18 14:19:03 +0900

    pyplot 함수로 그래프 만들기

--- practice2.py
+++ practice2.py
@@ -1,5 +1,10 @@
 from strategy import *
+import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib.ticker as ticker
+import pandas as pd
+
+
 useppo = UsePPO()
 # useoscillator = UseOSCILLATOR()
 kospibasket = useppo.kospibasket
@@ -16,13 +21,20 @@ for day in range(len(kospibasket)):
     yesterday = kospibasket[day - 1][0]
     todaydate = kospibasket[day][0]
     stocklist1 = useppo.CheckStrogStock(beforemonth, yesterday, 2)
-    graphdate = str(kospibasket[day][0])
+    graphdate = str(int(kospibasket[day][0]))
     print(todaydate, count)
     garo.append(graphdate)
     sero.append(useppo.MackoverlapPPO(day, 5, stocklist1))  # 여기에 할당해서 밑에 대입해야함
-    plt.scatter(garo, sero)
-
-
+    df = pd.DataFrame({
+        'DATE': garo,
+        'AMOUNT': sero
+    })
+    xs = df['DATE'].to_list()
+    xlabels = df['DATE'].apply(lambda x: x[2:]).to_list()
+    ys = df['AMOUNT'].to_list()
+    plt.plot(xs, ys)
+    plt.xticks(ticks=xs, labels=xlabels, rotation=45)
+    plt.locator_params(axis='x', nbins=int(len(xlabels) / 30))
     # useoscillator.PositiveOS_Day(day, yesterday, stocklist1)
     # useoscillator.NegativeOS_Day(day, stocklist1)
 
