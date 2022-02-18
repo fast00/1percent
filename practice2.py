2dfd5460383deb72de35358a96ba7a268dae3721
fast00 <74058894+fast00@users.noreply.github.com>
2022-02-18 09:14:35 +0900

    코스닥150 이격도 체크

--- practice2.py
+++ practice2.py
@@ -4,29 +4,28 @@ useppo = UsePPO()
 useoscillator = UseOSCILLATOR()
 kospibasket = useoscillator.kospibasket
 count = 0
-count2 = 0
+graphcount = 0
 garo = []
 sero = []
 for day in range(len(kospibasket)):
     count += 1
-    count2 += 1
+    graphcount += 1
     if day < 400:
         continue
     beforemonth = kospibasket[day - 20][0]
     yesterday = kospibasket[day - 1][0]
     todaydate = kospibasket[day][0]
-    todaydate2 = str(kospibasket[day][0])
-    print(todaydate, count)
     stocklist1 = useoscillator.CheckStrogStock(beforemonth, yesterday, 2)
-    # useoscillator.PositiveOS_Day(day, yesterday, stocklist1)
-    # useoscillator.NegativeOS_Day(day, stocklist1)
-    garo.append(todaydate2)
+    graphdate = str(kospibasket[day][0])
+    print(todaydate, count)
+    garo.append(graphdate)
     sero.append(useppo.MackoverlapPPO(day, 5, stocklist1))  # 여기에 할당해서 밑에 대입해야함
     plt.plot(garo, sero)
-    if count2 % 100 == 0:
+
+
+    # useoscillator.PositiveOS_Day(day, yesterday, stocklist1)
+    # useoscillator.NegativeOS_Day(day, stocklist1)
+
+    if graphcount % 100 == 0:
         plt.show()
 
-
-
-
-a = []
